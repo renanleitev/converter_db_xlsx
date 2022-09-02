ff8b871fef7fac6e9d3587f61b15e781381892ff
Renan Leite Vieira <109419403+renanleitev@users.noreply.github.com>
2022-09-02 12:30:34 -0300

    Update converter_xlsx_to_db.py

--- converter_xlsx_to_db.py
+++ converter_xlsx_to_db.py
@@ -1,13 +1,12 @@
 import sqlite3
 import pandas as pd
 
-
 con = sqlite3.connect("laura.db")
-wb=pd.ExcelFile(r'C:\Users\renan\Documents\Programação\VsCode\Acomp Processual - Renan.xlsx') # noqa
+# Editar o caminho do arquivo
+wb=pd.ExcelFile(r'C:\caminho_do_arquivo\arquivo_excel.xlsx') # noqa
 for sheet in wb.sheet_names:
-        df=pd.read_excel(r'C:\Users\renan\Documents\Programação\VsCode\Acomp Processual - Renan.xlsx', sheet_name=sheet) # noqa
+        df=pd.read_excel(r'C:\caminho_do_arquivo\arquivo_excel.xlsx', sheet_name=sheet) # noqa
         df.to_sql(sheet, con, index=False, if_exists="replace")
 con.commit()
 con.close()
-
-print('ok')
+print('Excel convertido em banco de dados com sucesso.')
