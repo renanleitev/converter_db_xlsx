8ef904ff4fb259d00012cc63698ef200c45f806c
Renan Leite Vieira <109419403+renanleitev@users.noreply.github.com>
2022-09-02 12:35:48 -0300

    Update modificar_base.py

--- modificar_base.py
+++ modificar_base.py
@@ -1,16 +1,11 @@
 # Base de Dados: https://docs.python.org/3/library/sqlite3.html
 import sqlite3
 # Criando a base de dados
-conexao = sqlite3.connect('laura.db')
+conexao = sqlite3.connect('nome_da_base_de_dados.db')
 # Conectando à base de dados
 cursor = conexao.cursor()
-# Para exibir os valores selecionados
-# for row in cursor.execute("SELECT CLIENTE, PROCESSO, ANDAMENTO FROM ADV_ANDRESSA"): # noqa
-#     print(row)
-# Inserindo valores (atualizando os dias parados)
-cursor.execute("UPDATE ADV_LAURA SET DIAS_PARADOS = JULIANDAY('now') - JULIANDAY(ÚLTIMA_MOVIMENTAÇÃO)") # noqa
-cursor.execute("UPDATE ADV_ANDRESSA SET DIAS_PARADOS = JULIANDAY('now') - JULIANDAY(ÚLTIMA_MOVIMENTAÇÃO)") # noqa
-# cursor.execute("UPDATE ADV_ALVES_PASSOS SET DIAS_PARADOS = JULIANDAY('now') - JULIANDAY(ÚLTIMA_MOVIMENTAÇÃO)") # noqa
+# Inserindo valores (diferença entre datas)
+cursor.execute("UPDATE NOME_DA_TABELA SET COLUNA_DA_TABELA_COM_DIFERENÇA_DIAS = JULIANDAY('now') - JULIANDAY(COLUNA_DA_TABELA_COM_DATA)") # noqa
 # Para salvar as alterações:
 conexao.commit()
 # Para encerrar as alterações:
